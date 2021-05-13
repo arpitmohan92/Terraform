c147e3ed066cb8cfb7908a8faf8b1866524ff766
arpitmohan92 <64588683+arpitmohan92@users.noreply.github.com>
2021-05-13 22:55:27 +0530

    Update launch-template.tf

--- launch-template.tf
+++ launch-template.tf
@@ -39,9 +39,9 @@ resource "aws_launch_template" "prod_template" {
   
   network_interfaces {
     delete_on_termination = true
-    security_groups = "aws_security_group.allow_traffic_from_lb.id"
-    subnet_id = "aws_subnet.AppServerSub1.id"
   } 
+  
+  vpc_security_group_ids = ["aws_security_group.allow_traffic_from_lb.id"]
   
   tag_specifications {
     resource_type = "instance"
